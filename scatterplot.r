846ab2f274d7f833c9d44f8459c9ef3cc8303e5c
meganstoppler <mstopple@uw.edu>
2022-05-21 09:16:54 -0700

    scatter plots

--- scatterplot.r
+++ scatterplot.r
@@ -2,12 +2,20 @@ library(dplyr)
 
 diabetes <- read.csv("/Users/meganstoppler/Downloads/diabetes_012_health_indicators_BRFSS2015.csv")
 
+# scatter plot comparing age vs BMI
+ggplot(data = diabetes_df, aes(x = Age)) +
+  geom_point(aes(y = BMI)) +
+  ggtitle("Age vs BMI") +
+  labs(x = "Age", y = "BMI")
+
+# if we wanted to clean up the graph by restricting age range
+#
 # new data frame to restrict age values
 age_BMI_df <- diabetes_df %>%
-  filter(age > 6 & age < 10.5)
+  filter(Age > 6 & Age < 10.5)
 
 # scatter plot comparing age vs BMI
-ggplot(data = age_BMI_df, aes(x = age)) +
+ggplot(data = age_BMI_df, aes(x = Age)) +
   geom_point(aes(y = BMI)) +
   ggtitle("Age vs BMI") +
   labs(x = "Age", y = "BMI")
